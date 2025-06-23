a9873d0c9326e763681adcfed5bf4668dd95e96e
Amir Khan <designingworking@gmail.com>
2025-06-23 11:13:57 -0700

    Day 1

---
+++ Day1/Hello.java
@@ -0,0 +1,10 @@
+package Java.Day1;
+
+public class Hello {
+    public static void main(String[] args) {
+        
+        // Our First Program 
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Day 1: <br>
+
+--> Install JDK <br>
+--> Install IDE IntelliJ Idea or VS Code <br>
+--> Create Our first java program <br>
